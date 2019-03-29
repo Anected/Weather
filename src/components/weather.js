913e6468f9355fb91f20cea26b9720887c25412a
Anected <anected@gmail.com>
2019-03-29 18:23:55 +0300

    'AddingImages'

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,20 +1,46 @@
 import React from 'react';
+import Solnechno from '../images/Solnechno.svg'
+import Dozd from '../images/Dozd.svg'
+import Groza from '../images/Groza.svg'
+import Pasmurno from '../images/Pasmurno.svg'
+import Sneg from '../images/Sneg.svg'
+import Tuman from '../images/Tuman.svg'
 
 
 class Weather extends React.Component {
-   capitalizeFirstLetter = (string) =>{
+    capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     };
+    chooseImg = () =>{
+        if (this.props.description === 'облачно' || this.props.description === 'пасмурно' ){
+            return Pasmurno}
+        else if (this.props.description === 'солнечно' || this.props.description === 'ясно' ){
+            return Solnechno
+        }else if (this.props.description === 'дождь' || this.props.description === 'сильный дождь' || this.props.description === 'слабый дождь' ){
+            return Dozd
+        }else if (this.props.description === 'гроза'){
+            return Groza
+        }else if (this.props.description === 'снег'){
+            return Sneg
+        }else if (this.props.description === 'туман'){
+            return Tuman
+        }
+    };
+
     render() {
         return (
             <div>
                 {this.props.city &&
-                <div>
-                    <p> Город: {this.props.city}</p>
-                    <p>{this.capitalizeFirstLetter(this.props.description)}</p>
-                    <p> Температура: {Math.ceil(this.props.temp)}°</p>
-                    <p>Скорость ветра: {this.props.wind} м/с</p>
-                    <p>Влажность: {this.props.humidity}%</p>
+                <div className='weather'>
+                    <div>
+                        <img className='img' src={this.chooseImg()} width="150" height="150" alt='Pogoda'/>
+                        <p className='temp'>{Math.ceil(this.props.temp)}°</p>
+                    </div>
+                    <p className='city'>{this.capitalizeFirstLetter(this.props.description)} </p>
+                    <p className='text'>Влажность: {this.props.humidity}%</p>
+                    <p className='text'>Ветер: {Math.ceil(this.props.wind)} м/с</p>
+
+
                 </div>}
             </div>
         )
