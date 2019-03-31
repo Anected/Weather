2b099a84b8a245a20368d21b5dbc0eb86713feb0
Anected <anected@gmail.com>
2019-03-31 14:16:45 +0300

    'moreInfo'

--- src/App.js
+++ src/App.js
@@ -10,24 +10,62 @@ const API_KEY = 'd022cd263149a8cbbae87cdb92c511b5';
 
 class App extends Component {
     state = {
-        cityName: null,
-        temp: null,
-        wind: null,
-        description: null,
-        humidity: null,
+        actual: {
+            cityName: null,
+            temp: null,
+            wind: null,
+            description: null,
+            humidity: null,
+        },
+        next: {
+            data: null,
+            temp: null,
+            description: null,
+        },
+        next2: {
+            data: null,
+            temp: null,
+            description: null,
+        },
+        next3: {
+            data: null,
+            temp: null,
+            description: null,
+        }
     };
 
     getWeather = (lat,lon) => {
         axios.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}9&APPID=${API_KEY}&units=metric&lang=ru`)
             .then(response => {
                 const WeatherData = response.data.list[0];
+                const WeatherData1 = response.data.list[1];
+                const WeatherData2 = response.data.list[2];
+                const WeatherData3 = response.data.list[3];
                 const CountryData = response.data.city;
+                console.log(response.data);
                 this.setState({
-                    cityName: CountryData.name,
-                    temp: WeatherData.main.temp,
-                    wind: WeatherData.wind.speed,
-                    description: WeatherData.weather[0].description,
-                    humidity: WeatherData.main.humidity
+                    actual:{
+                        cityName: CountryData.name,
+                        temp: WeatherData.main.temp,
+                        wind: WeatherData.wind.speed,
+                        description: WeatherData.weather[0].description,
+                        humidity: WeatherData.main.humidity
+                    },
+                    next: {
+                        data: WeatherData1.dt_txt,
+                        temp: WeatherData1.main.temp,
+                        description: WeatherData1.weather[0].description,
+                    },
+                    next2: {
+                        data: WeatherData2.dt_txt,
+                        temp: WeatherData2.main.temp,
+                        description: WeatherData2.weather[0].description,
+                    },
+                    next3: {
+                        data: WeatherData3.dt_txt,
+                        temp: WeatherData3.main.temp,
+                        description: WeatherData3.weather[0].description,
+                    },
                 })
 
             })
@@ -39,14 +77,23 @@ class App extends Component {
     render() {
         return (
             <div >
-                <Info  city={this.state.cityName}/>
-                <Form getWeather={this.getWeather} cityName={this.state.cityName}/>
+                <Info  city={this.state.actual.cityName}/>
+                <Form getWeather={this.getWeather} cityName={this.state.actual.cityName}/>
                 <Weather
-                    city={this.state.cityName}
-                    temp={this.state.temp}
-                    wind={this.state.wind}
-                    description={this.state.description}
-                    humidity={this.state.humidity}/>
+                    city={this.state.actual.cityName}
+                    temp={this.state.actual.temp}
+                    wind={this.state.actual.wind}
+                    description={this.state.actual.description}
+                    humidity={this.state.actual.humidity}
+                    temp1={this.state.next.temp}
+                    temp2={this.state.next2.temp}
+                    temp3={this.state.next3.temp}
+                    description1={this.state.next.description}
+                    description2={this.state.next2.description}
+                    description3={this.state.next3.description}
+                    data1= {this.state.next.data}
+                    data2= {this.state.next2.data}
+                    data3= {this.state.next3.data}/>
             </div>
         );
     }
--- src/components/weather.js
+++ src/components/weather.js
@@ -11,18 +11,19 @@ class Weather extends React.Component {
     capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     };
-    chooseImg = () =>{
-        if (this.props.description === 'облачно' || this.props.description === 'пасмурно' ){
-            return Pasmurno}
-        else if (this.props.description === 'солнечно' || this.props.description === 'ясно' ){
+    chooseImg = () => {
+        if (this.props.description === 'облачно' || this.props.description === 'пасмурно') {
+            return Pasmurno
+        }
+        else if (this.props.description === 'солнечно' || this.props.description === 'ясно') {
             return Solnechno
-        }else if (this.props.description === 'дождь' || this.props.description === 'сильный дождь' || this.props.description === 'слабый дождь' ){
+        } else if (this.props.description === 'дождь' || this.props.description === 'сильный дождь' || this.props.description === 'слабый дождь') {
             return Dozd
-        }else if (this.props.description === 'гроза'){
+        } else if (this.props.description === 'гроза') {
             return Groza
-        }else if (this.props.description === 'снег' || this.props.description === 'небольшой снегопад'){
+        } else if (this.props.description === 'снег' || this.props.description === 'небольшой снегопад') {
             return Sneg
-        }else if (this.props.description === 'туман'){
+        } else if (this.props.description === 'туман') {
             return Tuman
         }
         else {
@@ -43,7 +44,20 @@ class Weather extends React.Component {
                     <p className='text'>Влажность: {this.props.humidity}%</p>
                     <p className='text'>Ветер: {Math.ceil(this.props.wind)} м/с</p>
 
-
+                    <div className='card'>
+                        <div className='cardDiv'>
+                            <p className='cardElement '>Время : {this.props.data1}</p>
+                            <p className='cardElement '>{Math.ceil(this.props.temp1)}°  &nbsp; {this.capitalizeFirstLetter(this.props.description1)}</p>
+                        </div>
+                        <div className='cardDiv'>
+                            <p className='cardElement '>Время : {this.props.data2}</p>
+                            <p className='cardElement '>{Math.ceil(this.props.temp2)}°&nbsp; {this.capitalizeFirstLetter(this.props.description2)}</p>
+                        </div>
+                        <div className='cardDiv'>
+                            <p className='cardElement '>Время : {this.props.data3}</p>
+                            <p className='cardElement '>{Math.ceil(this.props.temp3)}°&nbsp; {this.capitalizeFirstLetter(this.props.description3)}</p>
+                        </div>
+                    </div>
                 </div>}
             </div>
         )
